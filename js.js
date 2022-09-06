4b9c04b78b525b8e635033c525e6e6d1b5769c9b
the69nemo <thestigmata@yandex.ru>
2022-09-06 20:02:44 +0300

    done

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-const slider = new Swiper('.bulding-slider', {
+const slider = new Swiper(".bulding-slider", {
   slidesPerView: 3,
   speed: 400,
   spaceBetween: 20,
@@ -8,3 +8,41 @@ const slider = new Swiper('.bulding-slider', {
   },
   loop: true,
 });
+
+const textSlider = new Swiper(".text-slider", {
+  slidesPerView: 1,
+  speed: 1400,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  parallax: true,
+});
+
+const select = () => {
+  const selectButtonList = document.querySelectorAll(".header__select-dropdown-btn");
+  const selectItemList = document.querySelectorAll(".header__select-item");
+  const inputNames = ["type", "location", "purpose"];
+
+  selectButtonList.forEach((selectButton) => {
+    selectButton.addEventListener("click", () => {
+      selectButton
+        .closest(".header__select-wrapper")
+        .classList.toggle("active");
+    });
+  });
+
+  selectItemList.forEach((selectItem) => {
+    selectItem.addEventListener("click", () => {
+      const dropDownMenu = selectItem.closest(".header__select-dropdown-menu");
+      const dropDownMenuClass = Array.from(dropDownMenu.classList);
+      if (inputNames.includes(dropDownMenuClass[1])) {
+        const input = dropDownMenu.nextElementSibling;
+        input.value = selectItem.textContent;
+      }
+      selectItem.closest(".header__select-wrapper").classList.remove("active");
+    });
+  });
+};
+
+select();
